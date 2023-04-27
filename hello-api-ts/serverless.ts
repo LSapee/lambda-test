01fa37bca76b2a62fe8b3852bc0d2f5822aec0df
Lsapee <dlworhks94@gmail.com>
2023-04-27 19:33:21 +0900

    lambda helloWorld

--- hello-api-ts/serverless.ts
+++ hello-api-ts/serverless.ts
@@ -1,38 +1,26 @@
-import type { AWS } from '@serverless/typescript';
+import type {AWS} from "@serverless/typescript";
 
-import hello from '@functions/hello';
-
-const serverlessConfiguration: AWS = {
-  service: 'hello-api-ts',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
-  provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
+const config : AWS = {
+    service :"hello-api-ts",
+    frameworkVersion: "3",
+    provider:{
+        name:"aws",
+        runtime:"nodejs14.x",
+        region:"ap-northeast-2",
     },
-    environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    functions:{
+        hello : {
+            handler: "handler.hello",
+            events:[
+                {
+                    httpApi: {
+                        path : "/hello",
+                        method : "get",
+                    },
+                },
+            ],
+        },
     },
-  },
-  // import the function via paths
-  functions: { hello },
-  package: { individually: true },
-  custom: {
-    esbuild: {
-      bundle: true,
-      minify: false,
-      sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
-      concurrency: 10,
-    },
-  },
+    plugins: ["serverless-webpack"],
 };
-
-module.exports = serverlessConfiguration;
+export =config
